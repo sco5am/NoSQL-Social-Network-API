65140db61e1c9397f447bf205c510e0e2eb318e7
Sam Oaks <samuelclayoaks@yahoo.com>
2023-03-14 18:32:55 -0400

    near final

--- server.js
+++ server.js
@@ -10,11 +10,7 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-API', {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-API');
 
 mongoose.set('debug', true);
 
